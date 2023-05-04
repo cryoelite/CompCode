ab97f195a4b5b860f7e181e08976122bba85e5a8
cryoelite <arvindsagar702@gmail.com>
2023-05-04 22:53:45 +0530

    Line Segment Intersection

---
+++ .ccls-cache/C@@NFBase@GitHub@CompCode/Helpers@Easybench@Easybench.h
@@ -0,0 +1,17 @@
+#ifndef EASYBENCH
+#define EASYBENCH
+// Header Guard
+
+#include <chrono>
+#include <string_view>
+class EasyBench
+{
+    std::chrono::steady_clock::time_point m_start;
+
+public:
+    EasyBench();
+    void showresult(std::string_view message = "");
+    void startTimer();
+};
+
+#endif
